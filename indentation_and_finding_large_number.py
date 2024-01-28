1f3f842c0408c7e3adb1270c3542a3913bbf55ab
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-01-28 19:29:17 +0530

    Create indentation_and_finding_large_number.py

---
+++ indentation_and_finding_large_number.py
@@ -0,0 +1,13 @@
+#indentation 
+#leading white spaces at the begining of a stetement is called indentation
+#inthe if else statements has two blocks of statements and the sytatements in each block are indented with the same amount of spaces or tabs
+
+#program tofind the larger of  the two numbers
+num1=5
+num2=6
+if num1>num2:
+    print("first number is larger")
+    prin("hai")
+else:
+    print("second number is laregr")
+    print("bye")
