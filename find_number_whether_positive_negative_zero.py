4ad879b88b55b1f593153b75599a9380da569bde
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-01-28 19:19:41 +0530

    Create find_number_whether_positive_negative_zero.py

---
+++ find_number_whether_positive_negative_zero.py
@@ -0,0 +1,8 @@
+#whether a number is positive or negative or zero
+number=int(input("enetr a number:"))
+if number>0:
+    print("number is positive")
+elif number<0:
+    print("number is negative")
+else:
+    print("number is zero")
