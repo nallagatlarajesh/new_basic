d9cdc057d14c4b8ec8c4e0ce2b7086d0ba453aea
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2024-02-04 23:06:33 +0530

    Create pandas1.py

---
+++ pandas1.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+labels=["a","b","c"]
+my_data=[10,20,30]
+arr=np.array(my_data)
+d={"a":10,"b":20,"c":30}
+
+print("l",labels)
+print(my_data)
+print(d)
+print(arr)
